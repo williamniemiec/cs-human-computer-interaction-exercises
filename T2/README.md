b0c52a44459e799138511512b32065a7c94f0fbd
William Niemiec <williamniemiec@hotmail.com>
2021-08-27 21:35:44 -0300

    T2

---
+++ T2/README.md
@@ -0,0 +1,8 @@
+# T2
+
+|Nome | Descrição |
+|-----|-----------|
+|ihc_t2_e1|Corresponde ao exercício 1 do `Enunciado 1.pdf`|
+|ihc_t2_e2|Corresponde ao exercício 2 do `Enunciado 1.pdf`|
+|ihc_t2_e3|Corresponde ao exercício 3 do `Enunciado 1.pdf`|
+|ihc_t2_e4|Corresponde ao exercício 1, 2 e 3 do `Enunciado 2.pdf`|
--- T2/ihc_t2_e4/App.js
+++ T2/ihc_t2_e4/App.js
@@ -1,112 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState, useEffect  } from 'react';
+import { View, Text, NativeModules, DeviceEventEmitter } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const mSensorManager = NativeModules.SensorManager;
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function HomeScreen() {
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [lighting, setLighting] = useState(0.0);
+  const [coords, setCoords] = useState({});
+  const [gyroscopeX, setGyroscopeX] = useState(0.0);
+  const [gyroscopeY, setGyroscopeY] = useState(0.0);
+  const [gyroscopeZ, setGyroscopeZ] = useState(0.0);
+
+  const locationFinder = async () => {
+    Geolocation.getCurrentPosition((info) => {
+      setCoords({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude
+      });
+    });
+  }
+
+  useEffect(() => {
+    locationFinder();
+  }, []);
+
+  
+  DeviceEventEmitter.addListener('Gyroscope', function (data) {
+    setGyroscopeX(data.x);
+    setGyroscopeY(data.y);
+    setGyroscopeZ(data.z);
+  });
+  mSensorManager.startGyroscope(100);
+
+  mSensorManager.startLightSensor(100);
+  DeviceEventEmitter.addListener('LightSensor', function (data) {
+    setLighting(data.light);
+  });
+  
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <View style={{margin: 30}}>
+      <Text>Latitude: {coords.latitude}; Longitude: {coords.longitude}</Text>
+      <Text>Lighting: {lighting}</Text>
+      <Text>Gyroscope:</Text>
+      <Text>
+        X: {gyroscopeX}
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+      <Text>
+        Y: {gyroscopeY}
+      </Text>
+      <Text>
+        Z: {gyroscopeZ}
       </Text>
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+}
